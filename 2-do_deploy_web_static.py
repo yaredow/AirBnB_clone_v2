e6b9ef5fb193b6e056b94faff968657559882f81
yaredow <yaredyilma11@gmail.com>
2022-03-24 11:37:40 -0700

    task 2 complated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
-""" a module to push a package to servers and deploy """
+"""
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
+"""
+
+from fabric.api import *
+from datetime import datetime
 import os
-from fabric.api import put, env, run
+
+filename = "web_static_" + datetime.now().replace(microsecond=0)\
+            .strftime("%Y%m%d%H%M%S") + ".tgz"
+
+env.hosts = ["ubuntu@34.138.32.10", "ubuntu@34.237.53.89"]
 
 
-env.hosts = ["35.231.213.145", "34.234.65.186"]
-
-env.user = "ubuntu"
+def do_pack():
+    """Fabric function"""
+    print("Packing web_static to versions/{}".format(filename))
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    local("tar -cvzf versions/{} web_static".format(filename))
+    print("web_static packed: versions/{} -> {}Bytes".format(
+        filename, os.path.getsize("versions/{}".format(filename))))
 
 
 def do_deploy(archive_path):
-    """ deploy package """
-    if archive_path is None or not os.path.isfile(archive_path):
-        print("NOT PATH")
+    """Fabric deploy function, takes a file path as argument"""
+    if not os.path.isfile(archive_path):
         return False
-
-    aname = os.path.basename(archive_path)
-    rname = aname.split(".")[0]
-
-    put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(rname))
-    run("tar -xzf /tmp/{} \
-        -C /data/web_static/releases/{}".format(aname, rname))
-    run("rm /tmp/{}".format(aname))
+    # remove .tgz extension
+    filename = archive_path.replace("versions/", "")
+    # remove versions folder path
+    folder_name = filename.replace(".tgz", "")
+    put(archive_path, "/tmp", use_sudo=True)
+    run("mkdir -p /data/web_static/releases/{}".format(folder_name))
+    run("tar -xzf /tmp/{} --strip-components 1 -C \
+        /data/web_static/releases/{}".format(filename, folder_name))
+    run("rm /tmp/{}".format(filename))
     run("rm -rf /data/web_static/current")
-    run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(rname))
-    run("mv /data/web_static/current/web_static/* /data/web_static/current/")
-    run("rm -rf /data/web_static/curren/web_static")
-
-    return True
+    run("ln -s /data/web_static/releases/{} /data/web_static/current".
+        format(folder_name))
